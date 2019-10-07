08b5635dac4668835a98aa7b6321db4e7d129c15
LancerRevX <nikitakirenkov@gmail.com>
2019-10-07 19:17:10 +0700

    1

--- index.php
+++ index.php
@@ -10,17 +10,57 @@
 <html lang="ru" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Randomizer</title
-        style=""
+        <title>Randomizer</title>
+        <style>
+            body
+            {
+                background-color: white;
+                color: blue;
+                font-family: arial;
+                font-size: 16px;
+                font-variant: small-caps;
+                font-weight: bold;
+            }
+
+            form
+            {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                text-align: center;
+            }
+
+            form *
+            {
+                margin-bottom: 8px;
+            }
+
+            form div
+            {
+                text-align: right;
+                margin-bottom: 0;
+            }
+
+            button
+            {
+                color: red;
+                font: inherit;
+                cursor: pointer;
+            }
+        </style>
     </head>
     <body>
-        Your random number: <?php echo $number; ?>
-        <form class="" method="post">
-            From
-            <input type="number" name="from" value=<?php echo $from; ?>>
-            To
-            <input type="text" name="to" value=<?php echo $to; ?>>
-            <input type="submit">
+        <form method="post">
+            <p>Your random number: <?php echo $number; ?></p>
+            <div>
+                <span>From</span>
+                <input type="number" name="from" value=<?php echo $from; ?>>
+                <br>
+                <span>To</span>
+                <input type="text" name="to" value=<?php echo $to; ?>>
+            </div>
+            <button type="submit">Submit</button>
         </form>
     </body>
 </html>
